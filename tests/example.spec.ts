70be04210faea195eccf3ee87198a2289fbf2f23
Noam Gaash <noam.gaash@applitools.com>
2023-10-12 16:57:47 +0300

    feat: expose test results url

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,16 +1,19 @@
-import { test } from '@playwright/test';
-import { Eyes } from '@applitools/eyes-playwright';
+import {writeFileSync} from "fs";
+import {Eyes} from "@applitools/eyes-playwright";
+import {test} from "@playwright/test";
 
 const myApiKey = process.env.EYES_API_KEY ?? null;
 
-test('check my website looks the same', async ({ page }) => {
-  if(myApiKey === null) throw new Error('EYES_API_KEY environment variable is not set');
+test("check my website looks the same", async ({page}) => {
+    if (myApiKey === null) {
+        throw new Error("EYES_API_KEY environment variable is not set");
+    }
 
-  const eyes = new Eyes();
-  eyes.setApiKey(myApiKey);
-  await eyes.open(page, 'My website', 'First test');
-  await page.goto('https://www.example.com/');
-  await eyes.checkWindow('Main Page');
-  const results = await eyes.close();
-
+    const eyes = new Eyes();
+    eyes.setApiKey(myApiKey);
+    await eyes.open(page, "My website", "First test");
+    await page.goto("https://www.example.com/");
+    await eyes.checkWindow("Main Page");
+    const results = await eyes.close();
+    writeFileSync("url.txt", results.url);
 });
